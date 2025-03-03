c8a37466700c227f11bc5025e88b7f443e10c07e
Nikolay Mazein <feynmaz@gmail.com>
2025-03-03 07:55:26 +0500

    add _stream to logs

--- cmd/main.go
+++ cmd/main.go
@@ -32,7 +32,7 @@ func main() {
 	cfg.App.Version = version
 
 	vmLogs := logger.NewVictoriaLogsWriter(cfg.Log.VictoriaUrl)
-	log, err := logger.New(cfg.Log.Level, vmLogs)
+	log, err := logger.New(cfg, vmLogs)
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to get logger")
 	}
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,9 +1,11 @@
 package logger
 
 import (
+	"fmt"
 	"io"
 	"os"
 
+	"github.com/fungicibus/inventory/config"
 	"github.com/rs/zerolog"
 )
 
@@ -11,14 +13,18 @@ type Logger struct {
 	zerolog.Logger
 }
 
-func New(level int, nonConsoleWriter io.Writer) (*Logger, error) {
+func New(cfg *config.Config, nonConsoleWriter io.Writer) (*Logger, error) {
 	var writer io.Writer = os.Stdout
 	if nonConsoleWriter != nil {
 		writer = zerolog.MultiLevelWriter(os.Stdout, nonConsoleWriter)
 	}
 
 	zerolog.MessageFieldName = "_msg"
-
-	logger := zerolog.New(writer).With().Timestamp().Logger().Level(zerolog.Level(level))
+	stream := fmt.Sprintf("app=%s,env=%s", cfg.App.Name, cfg.App.Env)
+	logger := zerolog.New(writer).With().
+		Str("_stream", stream).
+		Timestamp().
+		Logger().
+		Level(zerolog.Level(cfg.Log.Level))
 	return &Logger{logger}, nil
 }
